331d29ac371154a91fdb93844ab748d85e40338f
Murat Emir Selvi <muratemir.selvi3@gmail.com>
2024-03-26 17:52:02 +0300

    gson

---
+++ src/main/java/Library/Library.java
@@ -0,0 +1,12 @@
+package Library;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+public class Library {
+
+    private ArrayList<Book> books = new ArrayList<Book>();
+
+}
