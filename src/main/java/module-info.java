3c37556b61455e84c22abb0bfb67357e3056e3b4
Murat Emir Selvi <muratemir.selvi3@gmail.com>
2024-03-28 19:04:50 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.bookshooklibrary216 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.google.gson;
+    requires com.dlsc.formsfx;
 
     opens com.example.bookshooklibrary216 to javafx.fxml;
     exports com.example.bookshooklibrary216;
