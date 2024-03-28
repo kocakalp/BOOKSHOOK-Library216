d55655692f764d0e8ff1b3cb09f7a1ca7bd73f5b
Murat Emir Selvi <muratemir.selvi3@gmail.com>
2024-03-28 19:04:28 +0300

    .

--- src/main/java/Library/Library.java
+++ src/main/java/Library/Library.java
@@ -2,9 +2,7 @@ package Library;
 
 
 import java.util.ArrayList;
-import com.google.gson.Gson;
-import com.google.gson.JsonSyntaxException;
-import com.google.gson.reflect.TypeToken;
+
 public class Library {
     private ArrayList<Book> books = new ArrayList<Book>();
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.bookshooklibrary216 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires com.dlsc.formsfx;
+    requires com.google.gson;
 
     opens com.example.bookshooklibrary216 to javafx.fxml;
     exports com.example.bookshooklibrary216;
