841c3ce668fb847c44ee65aba03accf99ad094c4
Ediz Arkın Kobak <119970256+EdizArkin@users.noreply.github.com>
2024-04-15 21:05:22 +0300

    Defaultpng added

--- src/main/java/Library/Book.java
+++ src/main/java/Library/Book.java
@@ -41,7 +41,7 @@ public class Book implements Comparable<Book>  {
     }
 
     public Book() {
-        this("title", new ArrayList<>(), "subtitle", "author","publisher", "1", "0000000000", "1", "language" ,new ArrayList<>(),"1","bookk.png");
+        this("title", new ArrayList<>(), "subtitle", "author","publisher", "1", "0000000000", "1", "language" ,new ArrayList<>(),"1","default.png");
     }
 
 
@@ -71,7 +71,7 @@ public class Book implements Comparable<Book>  {
 
         } else {
             System.out.println("invalid Cover path");
-            setCoverPath("bookk.png");
+            setCoverPath("default.png");
         }
     }
     public void setTitle(String title) {
